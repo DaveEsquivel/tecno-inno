b079cf03ed38c576b47b349c5f20cd28eb3a2c42
Dave Esquivel <32052518+DaveEsquivel@users.noreply.github.com>
2017-10-05 21:04:02 -0500

    Ingreso del proyecto
    se agrego la carpeta que contiene el proyecto

---
+++ Prueba/Interfaz.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>  
+<html lang="en">  
+<head>  
+    <meta charset="UTF-8">
+    <title>David</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0 maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilos.css">
+    <script type="text/javascript" src="js/bootstrap.min.js" ></script>
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+</head>  
+<body> 
+<header>
+		<div class="container">
+			<h1>NOPALITO</h1>
+			<h3>Salud</h3>
+		</div>
+	</header>
+	
+	<div class="container">
+		<div class="main row">
+			<article class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
+				<p>
+					<h3>Embarazo a temprana edad :( </h3>
+					La mayoría de los embarazos en adolescentes son embarazos no deseados.
+El embarazo en una mujer adolescente puede llegar a término, produciéndose el nacimiento, o interrumpirse por aborto, ya sea esté espontáneo o inducido (aborto con medicamentos o aborto quirúrgico), en el caso del aborto inducido de manera legal o ilegal dependiendo de las leyes sobre el aborto de cada país.
+Aunque la OMS considera el embarazo adolescente un problema culturalmente complejo, alienta a retrasar la maternidad y el matrimonio para evitar los altos índices de mortalidad materna y mortalidad de recién nacidos, así como otras complicaciones sanitarias –parto prematuro, bajo peso, preeclampsia, anemia–, asociadas al embarazo en jóvenes adolescentes. En 2009 se estimaba que 16 millones de adolescentes, con edades comprendidas entre los 15 y 19 años, daban a luz cada año, un 95 % de esos nacimientos se producen en países en desarrollo, lo que representa el 11 % de todos los nacimientos en el mundo.
+				</p>
+				<div class="clearfix visible-lg-block"></div>
+			</article>
+		
+			<aside class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+				<p>
+					<h3>Qué es el ciclo menstrual?</h3>
+El ciclo menstrual o ciclo sexual femenino es el proceso que prepara al útero de la mujer para el embarazo todos los meses, mediante el desarrollo de los gametos femeninos y una serie de cambios fisiológicos. Se considera un ciclo contando desde el primer día de un periodo o menstruación hasta el primer día del periodo siguiente.
+				</p>
+			</aside>
+		</div>
+	<div class="clearfix visible-lg-block"></div>
+		<div class="row">
+			<div class="color1 col-xs-12 col-sm-6 col-md-3">
+				<h3>Enfermedades de Transmision sexual</h3>
+				<p>A veces referidos como infecciones de transmisión sexual (ITS), las enfermedades de transmisión sexual (ETS) son infecciones que se transmiten a través de contacto sexual. Las ETS son de las infecciones más comunes en los Estados Unidos. Cada año, millones de personas se infectan con una ETS.</p>
+			</div>
+			
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				<h3>Metodos Anticonseptivos</h3>
+				<p>Los métodos anticonceptivos son diferentes maneras de prevenir embarazos. Hay muchos métodos distintos que funcionan muy bien y son fáciles de usar. Entonces, ¿estás preparada para dejar de preocuparte por el embarazo? Estamos aquí para ayudarte a que comprendas todo.</p>
+			</div>
+
+			<div class="color1 col-xs-12 col-sm-6 col-md-3">
+				<h3>Mitos, costumbres y datos curiosos</h3>
+				<p>En éste apartado te presentaremos algunos de los tabúes más comunes y cómo han ido desapareciedo con el paso de las nuevas generaciones y la mejora de la educacion sexual hasta nuestros dias!</p>
+			</div>
+			<div class="clearfix visible-sm-block"></div>
+
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				<h3>Preguntas Frecuentes</h3>
+				<p>La juventud de hoy en dia muestra un gran inquietid a cerca de éste tema, ya que a pasar los años se ha convertido en un tema muy normal de hablarlo como practicarlo ¡aqui podemos ayudarte con tus dudas!</p>
+			</div>
+			<div class="clearfix visible-sm-block"></div>
+		</div>
+	</div>
+
+	<footer>
+		<div class="container">
+			<h3>ESQUIVEL RODRIGUEZ DAVID R.</h3>
+			<h4>Practica de Porcayo</h4>
+		</div>
+	</footer>
+	</div>
+	
+</body>
+</html>
---
+++ Prueba/prueba.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>  
+<html lang="en">  
+<head>  
+    <meta charset="UTF-8">
+    <title>Bootstrap desde cero</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0 maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <script type="text/javascript" src="js/bootstrap.min.js" ></script>
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+</head>  
+<body>  
+    <div class="container">
+        <h1 class="text-center">Mi nombre es Ofelia</h1>
+        <h2 class="text-center">¿Quién eres tú?</h2>
+        <p class="text-center lead">Yo! Yo he tenido tantos nombres, nombres viejos que solo pueden pronunciar el viento y los árboles. Yo soy el monte, y el bosque y la tierra. Soy soy un fauno, vuestro mas humilde servidor, alteza… </p>
+        <div class="text-center">
+            <button class="btn btn-primary">¡He visto un hada!</button>
+            <button class="btn btn-danger">¿Tú crees en las hadas?</button>
+        </div>
+    </div>
+</body>  
+</html>
